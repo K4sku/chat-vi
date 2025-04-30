af892f0a86785d9f9346abf485976cec71c1d6a1
Cezary Kłos <cezary.klos@gmail.com>
2025-04-30 22:54:33 +0200

    Add ruby_llm gem and create migrations for Chat, Messages, ToolCall models

---
+++ config/initializers/ruby_llm.rb
@@ -0,0 +1,7 @@
+require "ruby_llm"
+
+RubyLLM.configure do |config|
+  config.openai_api_key = ENV.fetch("OPENAI_API_KEY", nil)
+  config.anthropic_api_key = ENV.fetch("ANTHROPIC_API_KEY", nil)
+  config.gemini_api_key = ENV.fetch("GEMINI_API_KEY", nil)
+end
