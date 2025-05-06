93471b2b239c60d7d1c018c47fc45bc506361c2b
Cezary Kłos <cezary.klos@gmail.com>
2025-05-06 12:17:12 +0200

    Add title to chat model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_30_205307) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_06_093213) do
   create_table "chats", force: :cascade do |t|
     t.string "model_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   create_table "messages", force: :cascade do |t|
